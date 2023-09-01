63ae980d7a282fda6d27ae9d02d6ec4572dba933
wbposha <135736179+wbposha@users.noreply.github.com>
2023-09-01 16:39:26 +0400

    Update CashBackHackTest.java

--- src/test/java/ru/netology/service/CashBackHackTest.java
+++ src/test/java/ru/netology/service/CashBackHackTest.java
@@ -7,76 +7,8 @@ import org.testng.annotations.Test;
 public class CashBackHackTest {
 
     @Test
-    void shouldCalculateIfAmountLessThan1000 () {
+    void souldCashBackHackTest () {
+        CashBackHackService cashBackhackService = new CashBackHackService();
 
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = 900;
 
-        int actual = cashBackHackService.remain(amount);
-        int expected = 100;
-
-        Assert.assertEquals(actual, expected);
     }
-
-    @Test
-    void shouldCalculateIfAmountMoreThan1000 () {
-
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = 1100;
-
-        int actual = cashBackHackService.remain(amount);
-        int expected = 900;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldCalculateIfAmountEqual1000 () {
-
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = 1000;
-
-        int actual = cashBackHackService.remain(amount);
-        int expected = 0;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldCalculateIfAmountMoreThan5000 () {
-
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = 5900;
-
-        int actual = cashBackHackService.remain(amount);
-        int expected = 100;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldCalculateIfAmountEqual0 () {
-
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = 0;
-
-        int actual = cashBackHackService.remain(amount);
-        int expected = 1000;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldCalculateIfAmountLessThan0 () {
-
-        CashBackHackService cashBackHackService = new CashBackHackService();
-        int amount = -1000;
-
-        int actual = cashBackHackService.remain(amount);
-        int expected = 1000;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-
-}
